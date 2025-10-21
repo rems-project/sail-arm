0b344bf68b852c7fcbb87ca531f9a52849696cd3
Brian Campbell <Brian.Campbell@ed.ac.uk>
2025-10-21 14:09:40 +0100

    Update Rocq snapshot

--- arm-v9.4-a/snapshots/coq/arm_extras.v
+++ arm-v9.4-a/snapshots/coq/arm_extras.v
@@ -40,9 +40,10 @@
 
 Require Import SailStdpp.Base.
 Require Import Lia.
+Require Import armv9_types.
 Local Open Scope Z.
 
 (* A version of print_endline in the monad so that it doesn't disappear during
    extraction to OCaml.  A change in the Sail library declaration has to be
    spliced in to use it. *)
-Definition print_endline_monadic {rv e} (_:string) : monad rv unit e := returnm tt.
+Definition print_endline_monadic (_:string) : M unit := returnM tt.
